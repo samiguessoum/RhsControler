c7ea64fe24241458ccaf2e650133d0245edb6c25
guessoum <sami.guessoum@etu.u-paris.fr>
2026-01-31 02:41:41 +0100

    Initial commit (clean)

--- backend/dist/app.d.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-export declare const prisma: PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
-declare const app: import("express-serve-static-core").Express;
-export default app;
-//# sourceMappingURL=app.d.ts.map
